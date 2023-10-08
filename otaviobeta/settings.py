f13decfa67cb1b05532db71de837e97e45337d2a
jocasc <36902654+jocasc@users.noreply.github.com>
2023-10-08 19:41:09 +0100

    setup first pages

--- otaviobeta/settings.py
+++ otaviobeta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-4)mk)l)^n86h!kj97+byx_lcl*w67pd4r=^+*irlb)=j=%ut$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'gabsleal.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'gabsleal.pythonanywhere.com']
 
 
 # Application definition
@@ -55,7 +56,7 @@ ROOT_URLCONF = "otaviobeta.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
